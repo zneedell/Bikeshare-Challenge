c2f5839a13ab8bb6ca97c6786aa17e72bb715817
Zach Needell <zneedell@mit.edu>
2017-03-27 07:01:52 -0400

    change weekend def

--- Data/Preparation/makeJSONs.m
+++ Data/Preparation/makeJSONs.m
@@ -24,7 +24,7 @@ end
 
 
 %% Load Winter
-today = tripdata(~isweekend(tripdata.starttime) & tripdata.starttime > datetime(2015,12,1) & tripdata.starttime < datetime(2016,3,1),:);
+today = tripdata(~isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2015,12,21) & tripdata.starttime < datetime(2016,3,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -32,7 +32,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [ outTable, tripArray, stationstruct ] = addArrivalDeparture( today, stationstruct, startInd, stopInd );
 [stationstruct.W_WD] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:3,5,4:4]); stationstruct = rmfield(stationstruct,'StartStop');
 
-today = tripdata(isweekend(tripdata.starttime) & tripdata.starttime > datetime(2015,12,1) & tripdata.starttime < datetime(2016,3,1),:);
+today = tripdata(isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2015,12,21) & tripdata.starttime < datetime(2016,3,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -41,7 +41,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 
 [stationstruct.W_WE] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:4,6,5:5]); stationstruct = rmfield(stationstruct,'StartStop');
 %% Load Spring
-today = tripdata(~isweekend(tripdata.starttime)& tripdata.starttime > datetime(2016,3,1) & tripdata.starttime < datetime(2016,6,1),:);
+today = tripdata(~isweekend(tripdata.starttime+hours(3))& tripdata.starttime > datetime(2016,3,21) & tripdata.starttime < datetime(2016,6,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -49,7 +49,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [ outTable, tripArray, stationstruct ] = addArrivalDeparture( today, stationstruct, startInd, stopInd );
 [stationstruct.Sp_WD] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:5,7,6:6]); stationstruct = rmfield(stationstruct,'StartStop');
 
-today = tripdata(isweekend(tripdata.starttime)& tripdata.starttime > datetime(2016,3,1) & tripdata.starttime < datetime(2016,6,1),:);
+today = tripdata(isweekend(tripdata.starttime+hours(3))& tripdata.starttime > datetime(2016,3,21) & tripdata.starttime < datetime(2016,6,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -57,7 +57,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [ outTable, tripArray, stationstruct ] = addArrivalDeparture( today, stationstruct, startInd, stopInd );
 [stationstruct.Sp_WE] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:6,8,7:7]); stationstruct = rmfield(stationstruct,'StartStop');
 %% Load Summer
-today = tripdata(~isweekend(tripdata.starttime) & tripdata.starttime > datetime(2016,6,1) & tripdata.starttime < datetime(2016,9,1),:);
+today = tripdata(~isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2016,6,21) & tripdata.starttime < datetime(2016,9,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -65,7 +65,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [ outTable, tripArray, stationstruct ] = addArrivalDeparture( today, stationstruct, startInd, stopInd );
 [stationstruct.Su_WD] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:7,9,8:8]); stationstruct = rmfield(stationstruct,'StartStop');
 
-today = tripdata(isweekend(tripdata.starttime) & tripdata.starttime > datetime(2016,6,1) & tripdata.starttime < datetime(2016,9,1),:);
+today = tripdata(isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2016,6,21) & tripdata.starttime < datetime(2016,9,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -74,7 +74,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [stationstruct.Su_WE] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:8,10,9:9]); stationstruct = rmfield(stationstruct,'StartStop');
 %% Load Fall
 
-today = tripdata(~isweekend(tripdata.starttime) & tripdata.starttime > datetime(2016,9,1) & tripdata.starttime < datetime(2016,12,1),:);
+today = tripdata(~isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2016,9,21) & tripdata.starttime < datetime(2016,12,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
@@ -82,7 +82,7 @@ stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
 [ outTable, tripArray, stationstruct ] = addArrivalDeparture( today, stationstruct, startInd, stopInd );
 [stationstruct.F_WD] = stationstruct.StartStop; stationstruct = orderfields(stationstruct,[1:9,11,10:10]); stationstruct = rmfield(stationstruct,'StartStop');
 
-today = tripdata(isweekend(tripdata.starttime) & tripdata.starttime > datetime(2016,9,1) & tripdata.starttime < datetime(2016,12,1),:);
+today = tripdata(isweekend(tripdata.starttime+hours(3)) & tripdata.starttime > datetime(2016,9,21) & tripdata.starttime < datetime(2016,12,21),:);
 
 startInd = floor(nHourBins*hours(timeofday(today.starttime)))+1;
 stopInd = floor(nHourBins*hours(timeofday(today.stoptime)))+1;
